c150b5d9fde778e41e63de0e8daf3f717cf15ce4
AderitoOliveira <aderito.nelson1@gmail.com>
2020-11-18 20:45:21 +0000

    Inittial Commit

--- src/main/java/nuvemdesoftware/ceramicpro/authentication/JwtResponse.java
+++ src/main/java/nuvemdesoftware/ceramicpro/authentication/JwtResponse.java
@@ -5,13 +5,17 @@ import java.io.Serializable;
 public class JwtResponse implements Serializable {
 
     private static final long serialVersionUID = -8091879091924046844L;
+    private final String username;
     private final String jwttoken;
 
-    public JwtResponse(String jwttoken) {
+    public JwtResponse(String jwttoken, String username) {
+
         this.jwttoken = jwttoken;
+        this.username = username;
     }
 
     public String getToken() {
+
         return this.jwttoken;
     }
 }
